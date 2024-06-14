df6351e969daf4650d40a443e0b2d01faf5182cf
inesgcalvo <inglez@gmail.com>
2024-06-14 15:52:04 +0200

    feat: poll index

--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,3 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
 ]
-
-# from django.contrib import admin
-# from django.urls import include, path
-
-# urlpatterns = [
-#     path("polls/", include("polls.urls")),
-#     path("admin/", admin.site.urls),
-# ]
